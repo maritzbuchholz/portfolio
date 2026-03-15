dc072271597e31a9172e761c1b5390c0da58f223
maritzbuchholz <maritzbuchholz@gmail.com>
2026-03-15 19:01:14 -0400

    update footer styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,11 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer__content">
-                <p className="footer__text">© 2026 Maritz Buchholz. All rights reserved.</p>
+                <p className="footer__text">© 2026
+                <span className="footer-first"> R</span>
+                <span className="footer-last">it</span>
+                <span className="footer-first">z</span>.
+                All rights reserved.</p>
             </div>
         </footer>
     );
