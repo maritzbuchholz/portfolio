b9cd8ed171e20cf9bc7312358942d54084071a1e
maritzbuchholz <maritzbuchholz@gmail.com>
2026-03-15 16:02:21 -0400

    set up folder structure

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.scss';
+
+const LandingPage = () => {
+    return (
+        <div className="landing-page">
+            <main>
+            </main>
+        </div>
+    );
+};
+
+export default LandingPage;
